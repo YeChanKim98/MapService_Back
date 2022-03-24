bf198db21e50369db88854edf7e68fb5705a957b
YC98 <ssy5577@naver.com>
2022-03-25 00:05:03 +0900

    Back Init

---
+++ src/test/java/com/service/searchplay/SearchplayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.service.searchplay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SearchplayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
