9b69d1cc5798ea45a89be3f7331bafacbc5276bf
Jiyoung0711 <101436862+Jiyoung0711@users.noreply.github.com>
2022-05-09 14:33:58 +0900

    Add files via upload

---
+++ src/main/java/com/service/searchplay/User.java
@@ -0,0 +1,74 @@
+package com.example.user.domain;
+
+import com.sun.istack.NotNull;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+@Transactional
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+
+    private int seq;
+    @NotNull private String name;
+    @NotNull private String id;
+    @NotNull private String password;
+    private String nickname;
+    @NotNull private String email;
+    private String phone;
+    @NotNull private String age;
+
+    public int getSeq() {
+        return seq;
+    }
+    public void setSeq(int seq) {
+        this.seq = seq;
+    }
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getId() {
+        return id;
+    }
+    public void setId(String id) {
+        this.id = id;
+    }
+    public String getPassword() {
+        return password;
+    }
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    public String getNickname() {
+        return nickname;
+    }
+    public void setNickname(String nickname) {
+        this.nickname = nickname;
+    }
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public String getPhone() {
+        return phone;
+    }
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+    public String getAge() {
+        return age;
+    }
+    public void setAge(String age) {
+        this.age = age;
+    }
+}
